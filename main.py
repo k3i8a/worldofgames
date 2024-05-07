937d3599866b5b2f54d10f39494b13cb60452b52
k3i8a <145574571+k3i8a@users.noreply.github.com>
2024-05-07 09:27:53 -0400

    finished up the started code and did some main

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ screen = pygame.display.set_mode((display_width, display_height))
 def start_menu():
     screen.fill((0,0,0))
     font = pygame.font.SysFont('arial', 40)
-    title = font.render('World of Wonders', True, (255, 255, 255))
+    title_font = pygame.font.SysFont('Times New Roman', 60)
+    title = title_font.render('World of Wonders', True, (255, 255, 255))
     start_button = font.render('Start', True, (255, 255, 255))
-    screen.blit(title, (display_width/2 - title.get_width()/2), (display_height/2 - title.get_height()/2))
-    screen.blit(start_button, (display_width/2 - start_button.get_width()/2), (display_height/2 + start_button.get_height()/2))
+    quit_button = font.render('Quit', True, (255, 255, 255))
+    screen.blit(title, ((display_width/2) - (title.get_width()/2), ((display_height/2) - (title.get_height()/2)) - 50))
+    screen.blit(start_button, ((display_width/2) - (start_button.get_width()/2), (display_height/2) + (start_button.get_height()/2)))
+    screen.blit(quit_button,  ((display_width/2) - (quit_button.get_width()/2), ((display_height/2) - (quit_button.get_height()/2)) + 90))
     pygame.display.update()
 
 game_start = "start_menu"
@@ -25,6 +28,17 @@ while True:
             quit()
     if game_start == "start_menu":
         start_menu()
+    if game_start == "game":
+        keys = pygame.key.get_pressed()
+    if game_start == "start_menu":
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+
+            game_start = "game "
+            game_over = False
+
+
+
 
 
 
