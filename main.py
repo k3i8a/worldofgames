879ed451fed4d84e517c4c2b6af2d7fa64b0dc53
k3i8a <145574571+k3i8a@users.noreply.github.com>
2024-05-08 09:27:52 -0400

    Add files via upload
    Im starting to use tkinter which is a button used to click start and end, but I seem to get complications between so tmr try to figure out how to solve the problem.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from tkinter import *
+import threading
 import time
 import random
 
@@ -8,16 +10,40 @@ display_width = 800
 display_height = 600
 screen = pygame.display.set_mode((display_width, display_height))
 
+window = Tk()
+window.geometry("800x600")
+
+
+stop = False
+
+def ending_button():
+    global stop
+    stop = True
+
+def starting_button():
+    global stop
+    stop = False
+
+def button_starting():
+    t = threading.Thread(target = starting_button)
+    t.start()
 def start_menu():
     screen.fill((0,0,0))
-    font = pygame.font.SysFont('arial', 40)
+    # font = pygame.font.SysFont('arial', 40)
     title_font = pygame.font.SysFont('Times New Roman', 60)
     title = title_font.render('World of Wonders', True, (255, 255, 255))
     start_button = font.render('Start', True, (255, 255, 255))
-    quit_button = font.render('Quit', True, (255, 255, 255))
+    the_start = Button(window, text="START", padx=30, pady=20, command=button_starting)
+    the_start.grid(columnspan=1, row=1, column=0)
+    # quit_button = font.render('Quit', True, (255, 255, 255))
     screen.blit(title, ((display_width/2) - (title.get_width()/2), ((display_height/2) - (title.get_height()/2)) - 50))
-    screen.blit(start_button, ((display_width/2) - (start_button.get_width()/2), (display_height/2) + (start_button.get_height()/2)))
-    screen.blit(quit_button,  ((display_width/2) - (quit_button.get_width()/2), ((display_height/2) - (quit_button.get_height()/2)) + 90))
+    # screen.blit(start_button, ((display_width/2) - (start_button.get_width()/2), (display_height/2) + (start_button.get_height()/2)))
+    # screen.blit(quit_button,  ((display_width/2) - (quit_button.get_width()/2), ((display_height/2) - (quit_button.get_height()/2)) + 90))
+    # the_start = Button(window, text="START", padx=30, pady=20, command=button_starting)
+    # the_start.grid(columnspan=1, row=1, column=0)
+    the_end = Button(window, text="EXIT", padx=33, pady=22, command=ending_button)
+    the_end.grid(row=2, column=0)
+    window.mainloop()
     pygame.display.update()
 
 game_start = "start_menu"
@@ -28,14 +54,20 @@ while True:
             quit()
     if game_start == "start_menu":
         start_menu()
-    if game_start == "game":
-        keys = pygame.key.get_pressed()
-    if game_start == "start_menu":
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
+        # the_start = Button(window, text = "START", padx = 30, pady = 20, command = button_starting)
+        # the_start.grid(columnspan = 1, row = 1, column = 0)
+        # the_end = Button(window, text = "EXIT", padx = 33, pady = 22, command = ending_button)
+        # the_end.grid(row = 2, column = 0)
+        # window.mainloop()
 
-            game_start = "game "
-            game_over = False
+    # if game_start == "game":
+    #     keys = pygame.key.get_pressed()
+    # if game_start == "start_menu":
+    #     keys = pygame.key.get_pressed()
+    #     if keys[pygame.K_SPACE]:
+    #
+    #         game_start = "game "
+    #         game_over = False
 
 
 
